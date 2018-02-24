774c2e72295794452d472a94632fe1f3e4610d65
Fred Mitchell <fred.mitchell@gmx.de>
2018-02-24 21:31:36 +0100

    critter under construction

--- engine/headers/critter.h
+++ engine/headers/critter.h
@@ -1,4 +1,20 @@
 #pragma once
 
 namespace neat {
+  class Critter : public Neat {
+    class Genotype : public Neat {
+      struct Gene : public Neat {
+        std::size_t in_neuron;
+        std::size_t out_neuron;
+        double weight;
+
+        std::size_t innovation;
+
+        bool enabled;
+      };
+    };
+    
+    class Phenotype : public Neat {
+    };
+  };
 }
--- engine/headers/neat.h
+++ engine/headers/neat.h
@@ -1,9 +1,11 @@
 #pragma once
+#include <cstddef>
 #include <iostream>
 
 namespace neat {
   /* All NEAT type objects derive from this class.
    */
   class Neat {
+    virtual ~Neat() = default;
   }  
 }
