88949702d0d5ad07a17a85f374edbfe165437a87
Fred Mitchell <fred.mitchell@gmx.de>
2018-02-25 15:23:58 +0100

    added testing and benchmarks

--- engine/headers/critter.h
+++ engine/headers/critter.h
@@ -1,8 +1,20 @@
 #pragma once
+#include "neat.h"
 
 namespace neat {
   class Critter : public Neat {
     class Genotype : public Neat {
+      /* Gene Specification 
+       *
+       * The Gene specifies a singular input and output
+       * neuron, which represents a connection between
+       * them, along with the weight of that
+       * connection, which may be positive, negative,
+       * or zero.
+       * 
+       * There is also the enabled flag, which enables
+       * the gene.
+       */
       struct Gene : public Neat {
         std::size_t in_neuron;
         std::size_t out_neuron;
@@ -12,9 +24,14 @@ namespace neat {
 
         bool enabled;
       };
+
+      std::vector<Gene> genes;
     };
     
     class Phenotype : public Neat {
     };
+
+    Genotype genotype;
+    Phenotype phenotype;
   };
 }
